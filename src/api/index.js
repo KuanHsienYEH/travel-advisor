17f82f12ffca586c49b29d3337838a170082c707
sam <sam7.yeh@advantech.com.tw>
2022-07-16 13:40:45 +0800

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        "X-RapidAPI-Key": "6eeacecc2fmsh3e58d286580cfd6p181ed9jsnccd74f42375b",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    });
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find',
+      { params: {
+        lon: lng,
+        lat: lat,
+      },
+      headers: {
+        'X-RapidAPI-Key': '6eeacecc2fmsh3e58d286580cfd6p181ed9jsnccd74f42375b',
+        'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+      }})
+      return data;
+  } catch (error) {
+    console.log(error)
+  }
+}
